2757d5fe7760ef9be787cdc39c56a7e6c2187794
Zixuan Liu <zixuan.liu@entur.org>
2020-04-10 12:21:43 +0200

    Initial commit, website template using material design,  add test MakeObituary page

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MakeObituary from './pages/MakeObituary';
+import './App.css';
+
+function App() {
+  return <MakeObituary />;
+}
+
+export default App;
---
+++ src/components/MainContent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { Avatar } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import LayoutTextFields from "../components/LayoutTextFields";
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  title: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(3),
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  fullWidth: {
+    width: "100%",
+    height: "100%",
+  },
+  bigAvatar: {
+    margin: 30,
+    width: 160,
+    height: 160,
+  }
+}));
+
+function MainContent() {
+  const classes = useStyles();
+
+  return (
+    <main className={classes.fullWidth}>
+      <div className={classes.title}>
+        <Typography variant="h4">Create obituary</Typography>
+      </div>
+      <div className={classes.content}>
+        <Grid container justify="center" alignItems="center">
+          <Avatar src="/broken-image.jpg" className={classes.bigAvatar} />
+        </Grid>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus,
+          nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem
+          felis nec erat
+        </Typography>
+        <LayoutTextFields />
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
